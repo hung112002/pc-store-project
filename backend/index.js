75444965d504abf31e0f002e7e75ba52af33ecb2
nguyenmanhhung <hu.nguyenman@shimade.ac.jp>
2025-10-14 06:10:00 +0900

    Configure backend for deployment

--- backend/index.js
+++ backend/index.js
@@ -5,7 +5,7 @@ const cors = require('cors');
 const db = require('./db');
 
 const app = express();
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
